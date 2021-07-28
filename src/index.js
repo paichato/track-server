3b223145bf689f1aa016128732948efde3645f38
Marlon Strong <tsenane14@gmail.com>
2021-07-29 00:39:15 +0200

    mongoose is working

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
+
+// const mongoURI =
+//   "mongodb+srv://admin:ustro777@cluster0.ahmzk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to mongoose server!🎄");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log("Error connecting to mongo", err);
+});
 
 app.get("/", (req, res) => {
   res.send("Hi there!");
