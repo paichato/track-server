608d27b4e557832165993aabc981ea7948636630
Paichato <tsenane14@gmail.com>
2021-07-30 16:20:52 +0200

    missing requiAuth.js to finish

--- src/routes/authroutes.js
+++ src/routes/authroutes.js
@@ -20,4 +20,20 @@ router.post("/signup", async (req, res) => {
   //   console.log(req.body);
 });
 
+router.post('/signin', async (req,res)=>{
+  const {email,password}=req.body;
+  if(!email || !password){
+    return res.status(422).send({error:'Must provide email and password!'})
+  }
+  if(!email){
+    return res.status(404).send({error:'We dont have an account attached to that email'})
+  }
+  await User.comparePasswords(password).then(()=>{
+    const token=jwt.sign({userId:user._id},'MY_SECRET_KEY');
+    res.send({token});
+  }).catch((err)=>{
+    return res.status(422).send({error:'Invalid password or email'})
+  })
+})
+
 module.exports = router;
